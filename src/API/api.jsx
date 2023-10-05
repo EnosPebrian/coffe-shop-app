dc37ca4ffd01fee65d87fdbee454cecc5cc2d6ca
EnosPebrian <enospebrian1@gmail.com>
2023-10-05 09:58:10 +0700

    api switch

--- src/API/api.jsx
+++ src/API/api.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const API_URL = "https://the-coffee-space-api.crystalux.site";
+export const API_URL = "https://the-coffee-space-api.crystalux.web.id";
 // export const API_URL = "http://localhost:8003";
 export const api = axios.create({
   baseURL: API_URL,
